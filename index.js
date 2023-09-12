f7a546d4c9f44ada940a5026acca9b1409a9743b
Ayan Ahmad <newayanahmad@gmail.com>
2023-09-12 19:46:07 +0530

    Added APIs for login and register

---
+++ index.js
@@ -0,0 +1,128 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const sendOTP = require("./sendOTP");
+const { config } = require("dotenv");
+const bcrypt = require('bcryptjs');
+const User = require("./models/User")
+const mongoose = require('mongoose');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+config()
+// Create express app
+
+const app = express();
+app.use(cors())
+// Use body-parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set port
+const PORT = process.env.PORT || 3000;
+
+
+async function main() {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+}
+main().catch(err => console.log(err));
+async function createHash(value) {
+    const salt = await bcrypt.genSalt(10);
+    const hashedValue = await bcrypt.hash(value, salt);
+    return hashedValue;
+}
+
+async function checkHash(value, hashedValue) {
+    const isValid = await bcrypt.compare(value, hashedValue);
+    return isValid;
+}
+
+async function isEmailTaken(email) {
+    const user = await User.findOne({ email });
+    return !!user;
+}
+
+
+app.get("/", (req, res) => {
+    res.json({ "message": "Hello there!" })
+})
+
+app.post("/api/register", async (req, res) => {
+    const { name, email, password } = req.body;
+    const user = await User.findOne({ email })
+    if (user) {
+        res.json({ success: false, message: "Email already taken!" })
+        return;
+    }
+    let otp = Math.floor(100000 + Math.random() * 900000)
+    let hashedPassword = await createHash(password)
+    let hashedOTP = await createHash(String(otp))
+    const newUser = new User({ name: name.trim(), email: email.trim(), password: hashedPassword, otp: hashedOTP })
+    await newUser.save()
+    sendOTP(email, otp)
+    res.json({ success: true })
+})
+
+app.post("/api/verifyemail", async (req, res) => {
+    const { email, otp } = req.body;
+    const user = await User.findOne({ email: email });
+    let hashedOTP = user.otp
+    let userId = user._id.toString();
+    let isValid = await checkHash(otp, hashedOTP)
+    if (isValid) {
+        let user = await User.updateOne({ email: email }, { "$set": { isActive: true } })
+        let jwtToken = jwt.sign(userId, process.env.JWT_SECRET)
+        res.json({ success: true, token: jwtToken })
+    }
+    else {
+        res.json({ success: false, 'message': 'Invalid OTP' })
+    }
+})
+
+app.post("/api/verifyuser", async (req, res) => {
+    const { token } = req.headers;
+    try {
+        let isTokenValid = jwt.verify(token, process.env.JWT_SECRET)
+        if (isTokenValid) {
+            res.json({ userValid: true })
+        }
+        else {
+            res.json({ userValid: false })
+        }
+    }
+    catch {
+        res.json({ userValid: false })
+    }
+})
+
+
+app.post("/api/login", async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+        res.json({ success: false, message: "Invalid credentials" })
+        return;
+    }
+    let hashedPassword = user.password
+    let userId = user._id.toString();
+    let isValid = await checkHash(password, hashedPassword)
+    if (isValid) {
+        if (!user.isActive) {
+            let otp = Math.floor(100000 + Math.random() * 900000)
+            sendOTP(email, otp)
+            let hashedOTP = await createHash(String(otp))
+            res.json(({ success: true, verified: false }))
+            let user = await User.updateOne({ email: email }, { "$set": { otp: hashedOTP } })
+            return;
+        }
+        let jwtToken = jwt.sign(userId, process.env.JWT_SECRET)
+        res.json({ success: true, token: jwtToken, verified: true })
+    }
+    else {
+        res.json({ success: false, message: 'Invalid credentials' })
+    }
+})
+
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`App listening on http://localhost:${PORT}`);
+})
---
+++ sendOTP.js
@@ -0,0 +1,40 @@
+const nodemailer = require('nodemailer')
+const sendOTP = (to, otp) => {
+    let transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.MAIL_USERNAME,
+            pass: process.env.MAIL_PASSWORD,
+        },
+    });
+
+    transport.sendMail({
+        from: process.env.MAIL_USERNAME,
+        to: to,
+        subject: "Email verification",
+        html: `<body style="font-family: Arial, sans-serif; margin: 0; padding: 0;">
+        <div style="background-color: #f4f4f4; padding: 10px;">
+            <div style="max-width: 600px; margin: 0 auto;">
+                <div style="background-color: #fff; padding: 20px; text-align: center; border-radius: 15px; box-shadow: 0px 0px 10px rgba(0,0,0,0.1);">
+                    <h2 style="color: #444; font-size: 24px;">Welcome to Pizzila 🍕!</h2>
+                    <p style="font-size: 16px; color: #666;">
+                        We're excited to have you on board. To complete your registration, please verify your email address by entering the following OTP:
+                    </p>
+                    <div style="font-size: 24px; color: #444; padding: 20px; border-radius: 5px; background-color: #f9f9f9; display: inline-block;">
+                        ${otp}
+                    </div>
+                    <p style="font-size: 16px; color: #666;">
+                        If you did not request this code, you can safely ignore this email.
+                    </p>
+                    <p style="font-size: 16px; color: #666;">
+                        Thanks,<br>Pizzila Team
+                    </p>
+                </div>
+            </div>
+        </div>
+    </body>`
+    })
+}
+
+
+module.exports = sendOTP
